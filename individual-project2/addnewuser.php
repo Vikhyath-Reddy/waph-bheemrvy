3c20b3cc4920951175643a739afac45349b4b0fb
Vikhyath-Reddy <156843566+Vikhyath-Reddy@users.noreply.github.com>
2024-04-19 10:12:20 -0400

    Update addnewuser.php

--- individual-project2/addnewuser.php
+++ individual-project2/addnewuser.php
@@ -1,35 +1,102 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>User Page </title>
+      <link rel="stylesheet" href="design.css">
+   </head>
+   <body>
+   <div class="wrapper">
 <?php
 	$username = $_POST["username"];
 	$password = $_POST["password"];
-	if (isset($username) and isset($password)){
-		//echo "Debug> got username=$username;password=$password";
-		if(addnewuser($username,$password))
+	$email = $_POST["email"];
+	$name = $_POST["name"];
+	if(isset($username) and isset($password))
 	{
-		echo "Registration succeeded!";
+		//echo "Debug> got username=$username;password=$password";
+		$username=sanitize_input($_POST['username']);
+		$password=sanitize_input($_POST['password']);
+		$email=sanitize_input($_POST['email']);
+		$name = sanitize_input($_POST['name']);
+
+		#input length check
+		if(strlen($username) < 1 || strlen($password) < 8 || strlen($email)< 3 || strlen($name)<1)
+		{
+			?>
+            <div class="title">
+            Invalid Length for username : <?php echo htmlentities($username);?>!
+             </div>
+            <?php
+		}
+
+		//Reg exp check
+		else if ((!preg_match("/\w+/", $username)) || (!preg_match("/\w+/", $name))||
+		(!preg_match("/^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&])[\w!@#$%^&]{8,}$/",$password)) || 
+		(!preg_match("/^[\w.-]+@[\w-]+(\.[\w-]+)*$/", $email ))) 
+		{
+			?>
+            <div class="title">
+            Invalid pattern matching for username : <?php echo htmlentities($username);?>!
+             </div>
+            <?php
+		}
+
+		else if(addnewuser($name, $email,$username,$password))
+		{
+            ?>
+            <div class="title">
+            Registration successfull!! <?php echo htmlentities($username);?>!
+             </div>
+            <?php
+		}
+		else
+		{
+            ?>
+            <div class="title">
+            Registration FAILED!! <?php echo htmlentities($username);?>!
+             </div>
+            <?php
+			
+		}
 	}
 	else
 	{
-		echo "Registration failed!";
-	}
-	}else {
-		echo "No username/password provided!";
+        ?>
+            <div class="title">
+            No username/password provided!
+             </div>
+        <?php
 	}
 	
-	function addnewuser($username,$password)
+	function sanitize_input($input)
 	{
-		$mysqli = new mysqli('localhost','bheemrvy','Pa$$w0rd','waph');
+		$input=trim($input);
+		$input=stripslashes($input);
+		$input=htmlspecialchars($input);
+		return $input;
+	}
+
+	function addnewuser($name, $email,$username,$password)
+	{
+		$mysqli = new mysqli('localhost','bopparsr','Shruti@123','waph');
 		if($mysqli->connect_errno)
 		{
 			printf("Database connection failed: %s\n", $mysqli->connect_error);
-			return FALSE;
+			exit();
 		}
 
-		$prepared_sql = "INSERT INTO users (username,password) VALUES (?,md5(?));";
+		$sql = "INSERT INTO users(name, email,username,password) VALUES (?,?,?, md5(?));";
 		//$sql = $sql . " AND password = md5('". $password."')";
 		//echo "DEBUG > sql= $sql";
-		$stmt=$mysqli->prepare($prepared_sql);
-		$stmt->bind_param("ss",$username,$password);
-		if($stmt->execute()) return TRUE;
+		$stmt=$mysqli->prepare($sql);
+		$stmt->bind_param("ssss",$name, $email,$username,$password);
+		if($stmt->execute())
+			return TRUE;
 		return FALSE;
 	}
 ?>
+</div>
+   </body>
+</html>s
