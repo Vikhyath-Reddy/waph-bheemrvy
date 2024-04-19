d692ed460a6167ab8e0631f65bc61de4210eeb93
Vikhyath-Reddy <156843566+Vikhyath-Reddy@users.noreply.github.com>
2024-04-19 10:13:08 -0400

    Update changepassword.php

--- individual-project2/changepassword.php
+++ individual-project2/changepassword.php
@@ -1,37 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design.css">
+    <title>User Details</title>
+</head>
+<body>
+<div class="headingsContainer">
+            <h3>Passsword Status</h3>
+        </div>
+	<form>
 <?php
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	if (isset($username) and isset($password)){
-		echo "Debug> changepassword.php got username=$username;password=$password";
-		/*if(addnewuser($username,$password))
+	require "session_auth.php";
+	$username = $_SESSION['username'];
+	$password = $_REQUEST["newpassword"];
+	$token=$_POST["nocsrftoken"];
+	if(!isset($token) or ($token!==$_SESSION["nocsrftoken"]))
 	{
-		echo "Registration succeeded!";
+		echo "CSRF Attack is detected";
+		die();
+	}
+	if(isset($username) and isset($password))
+	{
+		
+		if(changepassword($username,$password))
+		{
+            ?>
+            <div class="mainContainer">
+            <h2>Password has been changed!!! for <?php echo htmlentities($username);?>!</h2>
+
+			<p class="register"> <a href="logout.php">Logout</a></p>
+             </div>
+            <?php
+		}
+		else
+		{
+			?>
+            <div class="mainContainer">
+            <h2>Change password failed!!!! for <?php echo htmlentities($username);?>!</h2>
+            
+	<p class="register"> <a href="logout.php">Logout</a></p>
+             </div>
+            <?php
+		}
 	}
 	else
 	{
-		echo "Registration failed!";
-	}*/
-	}else {
-		echo "No username/password provided!";
+		?>
+            <div class="mainContainer">
+            <?php echo "No username/password provided!";?>!
+             </div>
+            <?php
+		
 	}
 	
-	function addnewuser($username,$password)
+
+	function changepassword($username,$password)
 	{
-		$mysqli = new mysqli('localhost','atmakuma','N@idu898','waph');
+		$mysqli = new mysqli('localhost','bopparsr','Shruti@123','waph');
 		if($mysqli->connect_errno)
 		{
 			printf("Database connection failed: %s\n", $mysqli->connect_error);
-			return FALSE;
+			exit();
 		}
 
-		$prepared_sql = "INSERT INTO users (username,password) VALUES (?,md5(?));";
-		//$sql = $sql . " AND password = md5('". $password."')";
-		//echo "DEBUG > sql= $sql";
-		$stmt=$mysqli->prepare($prepared_sql);
-		$stmt->bind_param("ss",$username,$password);
-		$stmt->execute();
-		if($result -> num_rows==1)
+		$sql = "UPDATE users SET password=md5(?) WHERE username=?;";
+		$stmt=$mysqli->prepare($sql);
+		$stmt->bind_param("ss",$password,$username);
+		if($stmt->execute())
 			return TRUE;
 		return FALSE;
 	}
-?>
+
+	$token.die();
+	?>
+	</form>
+    </div>
+   </body>
+</html>
