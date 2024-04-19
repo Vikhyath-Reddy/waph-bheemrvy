aa234576072d38d88c13228e7713afe29bf49427
Vikhyath-Reddy <156843566+Vikhyath-Reddy@users.noreply.github.com>
2024-04-19 10:13:42 -0400

    Update changepasswordform.php

--- individual-project2/changepasswordform.php
+++ individual-project2/changepasswordform.php
@@ -1,27 +1,32 @@
+<?php
+	require "session_auth.php";
+	$rand = bin2hex(openssl_random_pseudo_bytes(16));
+	$_SESSION["nocsrftoken"]=$rand;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>WAPH-Change Password</title>
-  <script type="text/javascript">
-      function displayTime() {
-        document.getElementById('digit-clock').innerHTML = "Current time:" + new Date();
-      }
-      setInterval(displayTime,500);
-  </script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design.css">
+    <title>Change Password Form</title>
 </head>
 <body>
-  <h1>Change password, WAPH</h1>
-  <h2>Vikhyath Reddy Bheemreddy</h2>
-  <div id="digit-clock"></div>  
-<?php
-  //some code here
-  echo "Visited time: " . date("Y-m-d h:i:sa")
-?>
-  <form action="changepassword.php" method="POST" class="form login">
-    Username:<input type="text" class="text_field" name="username" /> <br>
-    Password: <input type="password" class="text_field" name="password" /> <br>
-    <button class="button" type="submit">Submit</button>
-  </form>
-</body>
+    <h1>Change Password Form, WAPH</h1>
+         <form action="changepassword.php" method="POST">
+            <div class="mainContainer">
+               <label for="username">Username : </label> <?php echo htmlentities($_SESSION['username']);?>
+               <input type="hidden" name="nocsrftoken" value="<?php echo $rand; ?>"/>
+               <br><br>
+
+               <label for="password">Password : </label>
+                <input type="password" name="newpassword" required>
+             
+                
+                <button type="submit">Change Password</button>
+               </div>
+         </form>
+        </div>
+    </body>
 </html>
